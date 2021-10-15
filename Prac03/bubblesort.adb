ea4e8b083be410f86d8efc2391e1952758133316
Tawana <tawanakwaramba@gmail.com>
2021-10-15 21:06:13 +0800

    need to finish  the bubble sort in ADA

--- Prac03/bubblesort.adb
+++ Prac03/bubblesort.adb
@@ -3,31 +3,31 @@ use Ada.Text_IO;
 
 
 procedure bubblesort is
-    --  defining a type of element for the programme
-    type My_Int is range 0 .. 1000;
-    -- the variable index is going to be be used as the bounds of the arar
-    type size is range 1 .. 10;
-
     end_sort : Integer  := 10;
     beginning_sort : Integer := 0;
 
-    type  myArray is array (size) of My_Int;
-    -- intitialising an array of 10 elements in reverse order
-    Arr : myArray := (10, 9, 8, 7, 6, 5, 4, 3, 2, 1);
+    type  myArray is array (0 .. 10) of Integer;
+    type myArray_access is access  myArray;
 
-procedure swap(firstValue : Integer; endValue : in Integer; inArray : inArray) is 
+    -- intitialising an array of 10 elements in reverse order
+    myArray_access : myArray_access; 
+    elements : myArray := (11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1);
+
+procedure swap(index : Integer; inArray : in myArray) is
+    temp : Integer;
 begin
-    Put_Line ("say hello");
+    temp := inArray(index);
+    inArray(index) := inArray(index + 1);
+    inArray(index + 1 ) := temp;
 end swap;
 
 
 begin 
-
+    swap(1, elements);
     for ii in Integer range beginning_sort .. end_sort loop
         for jj in Integer range beginning_sort .. end_sort - 1 loop
-            swap(10, 20);
+            Put_Line("hello nigger");
         end loop;
 
     end loop;
-
 end bubblesort;
