b5dbefd6cfd0a5adc2cfcd350937791b79cf0c1d
Tawana <tawanakwaramba@gmail.com>
2021-10-21 00:33:26 +0800

    can find all the config files in ruby

--- Prac05/findConf.rb
+++ Prac05/findConf.rb
@@ -1,31 +1,8 @@
 #!/usr/bin/env ruby
 require 'find'
-
-current_directory = Dir.pwd
-save_location = current_directory + "/out_ruby.file"
-
-
 #making sure that I only access my student id, change to the home directory of
 #personnal machine
 #outContents =  Dir["/home/19476700/**/*.conf*"]
 #puts outContents
 
-Find.find("/home/19476700/") { |f| puts f }
-
-def write_file(name)
-end
-
-
-def read_file(name)
-    file = File.open(name)
-    #removing the new line characters from the lines whcih have being read
-    fileContnets = file.readlines.map(&:chomp)
-    file.close
-
-    return  fileContnets
-end
-
-def printList(inList)
-    inList.each{|ii| puts ii}
-end
-
+Find.find("/home/19476700/") { |f| puts f if  f.include? ".conf"}
